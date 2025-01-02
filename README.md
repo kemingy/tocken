eb29566074aa4dc9c466c5b3cee1188c29104a89
Keming <kemingy94@gmail.com>
2025-01-02 12:17:55 +0800

    feat: init the crate
    Signed-off-by: Keming <kemingy94@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Tocken
 
-BM25 WAND implementation in Rust.
+Tokenizer implemented in Rust.
+
+This tokenizer is based on [Lucene's EnglishAnalyzer](https://github.com/apache/lucene/blob/525b963be076fe8c58dd1162f083b6a9911e4efd/lucene/analysis/common/src/java/org/apache/lucene/analysis/en/EnglishAnalyzer.java#L37).
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,41 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
+use argh::FromArgs;
+use env_logger::Env;
+use log::debug;
 use tocken::tokenizer::Tokenizer;
 
+#[derive(FromArgs, Debug)]
+/// Tokenize text file
+struct Args {
+    /// input file
+    #[argh(option, short = 'i')]
+    input: String,
+    /// output file
+    #[argh(option, short = 'o')]
+    output: String,
+    /// min frequency
+    #[argh(option, short = 'f', default = "1")]
+    min_freq: u32,
+}
+
 fn main() {
+    let args: Args = argh::from_env();
+
+    let env = Env::default().filter_or("TOCKEN_LOG", "debug");
+    env_logger::init_from_env(env);
+    debug!("{:?}", args);
+
     let mut tokenizer = Tokenizer::default();
-    let file = File::open("wiki.txt").expect("file not found");
+    tokenizer.min_freq = args.min_freq;
+    let file = File::open(args.input).expect("file not found");
     let reader = BufReader::new(file);
     let mut contents = Vec::new();
     for line in reader.lines() {
         contents.push(line.unwrap());
     }
-    println!("fit");
     tokenizer.fit(&contents);
-    println!("dump");
-    tokenizer.dump(&Path::new("wiki_tocken.json"));
+    tokenizer.trim();
+    tokenizer.dump(&Path::new(&args.output));
 }
